6b97e68f0b202b9634a17f71deca71473c8ed058
SidorovaVika <vikasi13@mail.ru>
2020-03-02 11:06:49 +0300

    fifth

--- project/models/user_department.py
+++ project/models/user_department.py
@@ -6,7 +6,7 @@ class UserDepartment(db.Model):
     __tablename__ = 'user_department'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False)
-    department_id = db.Column(db.Integer, nullable=False)
+    department_id = db.Column(db.Integer, nullable=True)
     post=db.Column(db.VARCHAR(200), nullable=False)
     employment_date=db.Column(db.Date, nullable=False)
     dismissal_date=db.Column(db.Date, nullable=True)
